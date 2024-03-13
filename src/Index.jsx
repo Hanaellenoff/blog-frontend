a479154a2a96d9380331ee7383c65aad0b91586f
Hanaellenoff <hanaellenoff@gmail.com>
2024-03-13 13:59:21 -0500

    editing css

--- src/Index.jsx
+++ src/Index.jsx
@@ -2,13 +2,15 @@ export function Index(props) {
   console.log(props);
   return (
     <div id="posts-index">
-      {props.posts.map((post) => (
-        <div key={post.id} className="posts">
-          <h1>The title is {post.title}</h1>
-          <p>{post.body}</p>
-          <img src={post.image} alt="" />
-        </div>
-      ))}
+      <div className="cards">
+        {props.posts.map((post) => (
+          <div key={post.id} className="posts card">
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            <img src={post.image} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/New.jsx
+++ src/New.jsx
@@ -2,11 +2,10 @@ export function New() {
   return (
     <div>
       <div id="posts-new">
-        <h1>A blog by Hana</h1>
-        <p>What to do when you are bored</p>
-        <img src="https://wallpapers.com/images/featured/coolest-pictures-88c269e953ar0aw4.jpg" width="500" />
+        <h1>What to do when your bored</h1>
+        <p>The only rule:</p>
         <a href="https://www.youtube.com/watch?v=YJPHK5NNtpQ">
-          <button>just dont tell me plutos not a planet</button>
+          <button>dont tell me plutos not a planet</button>
         </a>
       </div>
     </div>
