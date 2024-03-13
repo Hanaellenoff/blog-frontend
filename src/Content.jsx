f1a575a40526aa27379d0b4e0485a3f930f0e694
Hanaellenoff <hanaellenoff@gmail.com>
2024-03-13 15:57:56 -0500

    editing content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,29 +1,17 @@
 import { Index } from "./Index";
 import { New } from "./New";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export function Content() {
-  let posts = [
-    {
-      id: 1,
-      title: "Go for a ride",
-      body: "weeee",
-      image:
-        "https://imgd.aeplcdn.com/664x374/n/cw/ec/101487/classic-350-2021-right-front-three-quarter-4.jpeg?isig=0&q=80",
-    },
-    {
-      id: 2,
-      title: "Ship",
-      body: "ahoy",
-      image: "https://cdn.britannica.com/78/196578-131-B0FC5325/harbor-Pirate-Ship-way-campaign-marines-oceans.jpg",
-    },
-    {
-      id: 3,
-      title: "Tacos",
-      body: "are super good",
-      image:
-        "https://brandsitesplatform-res.cloudinary.com/image/fetch/w_1540,c_scale,q_auto:eco,f_auto,fl_lossy,dpr_1.0,e_sharpen:85/https://assets.brandplatform.generalmills.com%2F-%2Fmedia%2Fproject%2Fgmi%2Foldelpaso%2Foldelpaso-us%2Frecipes%2Fqtcu578og0gukdk_kb_rmg_gmi_hi_res_jpeg.jpeg%3F%201540w",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  const handleIndexPosts = () => {
+    axios.get("http://localhost:3000/posts.json").then((response) => {
+      setPosts(response.data);
+      useEffect(handleIndexPosts, []);
+      console.log(posts);
+    });
+  };
   return (
     <div>
       <New />
