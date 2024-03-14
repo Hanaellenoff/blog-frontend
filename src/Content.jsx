10a280e98ad6a6100a83c9f624ea9ea70729faaf
Hanaellenoff <hanaellenoff@gmail.com>
2024-03-14 11:36:40 -0500

    added modal

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,13 +2,14 @@ import { Index } from "./Index";
 import { New } from "./New";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Modal } from "./Modal";
 
 export function Content() {
   const [posts, setPosts] = useState([]);
   const handleIndexPosts = () => {
     axios.get("http://localhost:3000/posts.json").then((response) => {
       setPosts(response.data);
-      // console.log(posts);
+      console.log(posts);
     });
   };
   useEffect(handleIndexPosts, []);
@@ -16,6 +17,9 @@ export function Content() {
     <div>
       <New />
       <Index posts={posts} />
+      <Modal show={true}>
+        <p>TEST</p>
+      </Modal>
     </div>
   );
 }
